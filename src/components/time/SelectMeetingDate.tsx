3e5e8f923497e201a57c29f95c11152cb40d9413
devoncats <jivasquezr2021@gmail.com>
2024-06-19 00:31:44 -0500

    refactor: Update SelectMeetingTime component to use avaliability prop

--- src/components/time/SelectMeetingDate.tsx
+++ src/components/time/SelectMeetingDate.tsx
@@ -1,26 +1,55 @@
 import { useEffect } from 'react'
 import { useStore } from '@nanostores/react'
 
-import { Calendar } from '@nextui-org/react'
-import { today } from '@internationalized/date'
-import { timezone, date, setDate } from '@/stores/dateStore'
+import { Calendar, type DateValue } from '@nextui-org/react'
+import { parseDate, today } from '@internationalized/date'
+import { timezone, setDate, date } from '@/stores/dateStore'
 
-export default function SelectMeeting() {
+export default function SelectMeeting({ avaliability }: { avaliability: Avaliability[] }) {
     const userTimezone = useStore(timezone)
     const userDate = useStore(date)
-    const base = today(userTimezone)
+
+    const avaliableDates = avaliability.map((date) => date.day)
+
+    const invalidDate = (date: DateValue) => {
+        return !avaliableDates.includes(date.toString())
+    }
+
+    const firstAvaliableDateFromToday = () => {
+        const todayDate = today(userTimezone)
+        const avaliableDate = avaliableDates.find((date) => date >= todayDate.toString())
+
+        return avaliableDate ? parseDate(avaliableDate) : todayDate
+    }
+
     useEffect(() => {
-        setDate(today(userTimezone))
-    }, [userTimezone])
+        const queryParams = new URLSearchParams(window.location.search)
+        const dateParam = queryParams.get('date')
+
+        dateParam ? setDate(parseDate(dateParam)) : setDate(firstAvaliableDateFromToday())
+    }, [])
+
+
+    const handleSelectionChange = (event: DateValue) => {
+        const newDate = event.toString()
+
+        setDate(parseDate(newDate))
+
+        const queryParams = new URLSearchParams(window.location.search)
+        queryParams.set('date', newDate)
+        window.history.replaceState(null, '', `${window.location.pathname}?${queryParams.toString()}`)
+    }
+
     return (
         <>
             <h1 className='font-medium text-lg'>Select date and time</h1>
             <Calendar
                 className='shadow-none border'
                 color='secondary'
-                defaultValue={userDate}
-                minValue={base}
-                onChange={setDate}
+                isDateUnavailable={invalidDate}
+                defaultValue={firstAvaliableDateFromToday()}
+                onChange={handleSelectionChange}
+                minValue={today(userTimezone)}
             />
         </>
     )
--- src/components/time/SelectMeetingTime.tsx
+++ src/components/time/SelectMeetingTime.tsx
@@ -1,16 +1,12 @@
 import { useStore } from '@nanostores/react'
 import { date, timezone } from '@/stores/dateStore'
 
-export default function SelectMeetingTime({
-    avaliability,
-}: {
-    avaliability: Avaliability[]
-}) {
+export default function SelectMeetingTime({ avaliability }: { avaliability: Avaliability[] }) {
     const userDate = useStore(date)
     const userTimezone = useStore(timezone)
 
     const dayAvaliability = avaliability.filter(
-        (hours) => hours.day === userDate.toString()
+        (date) => date.day === userDate.toString()
     )
 
     const hourAvaliability = dayAvaliability.map((hours) => hours.hours)
@@ -19,17 +15,17 @@ export default function SelectMeetingTime({
 
     return (
         <>
-            {userDate.toDate(userTimezone).toUTCString()}
+            {userDate.toDate(userTimezone).toUTCString().slice(0, 16)}
             <div className='flex flex-col'>
                 {hourAvaliability.map((hours, index) => (
                     <div
-                        className='flex flex-col'
+                        className='grid grid-cols-2 gap-2'
                         key={index}
                     >
                         {hours.map((hour, index) => (
                             <button
                                 key={index}
-                                className='border border-gray-200 rounded p-2 m-1'
+                                className='border border-gray-200 rounded w-32 p-2 m-1'
                             >
                                 {hour}
                             </button>
